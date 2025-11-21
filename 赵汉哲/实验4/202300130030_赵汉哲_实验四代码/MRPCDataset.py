d0fa630f227b7669b0b98b529856082992792bf3
Chihiro <145273824+chihiro0610@users.noreply.github.com>
2025-11-21 11:59:15 +0800

    Add files via upload

--- 赵汉哲/实验4/202300130030_赵汉哲_实验四代码/MRPCDataset.py
+++ 赵汉哲/实验4/202300130030_赵汉哲_实验四代码/MRPCDataset.py
@@ -9,10 +9,9 @@ class MRPCDataset(Dataset):
             header = next(f)  # 跳过表头
             for i, line in enumerate(f, start=2):
                 cols = line.strip().split("\t")
-                # 处理某些句子中自带 tab 导致列数 > 5 的情况
                 label = int(cols[0])
                 s1 = cols[3]
-                # 剩余所有列合并为 sentence2（避免句子中含有 tab 的问题）
+                # 将多出的列合并到s2中
                 s2 = " ".join(cols[4:])
                 self.sentences.append(s1 + " [SEP] " + s2)
                 self.labels.append(label)
